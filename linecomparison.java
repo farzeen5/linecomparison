17f1115e60fa55c91e53dab4109a00cd7ca78731
Farzeen <farzeenkhan1412@gmail.com>
2021-07-27 10:37:28 +0530

    firstcommit

---
+++ linecomparison.java
@@ -0,0 +1,10 @@
+package linecomparison;
+
+public class linecomparison {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to line comaparison programme");
+	}
+
+}
